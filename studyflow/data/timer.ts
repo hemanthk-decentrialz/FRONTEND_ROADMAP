621922eb0f3c74b40232a5a5ce0a56e4d4eff7ed
hemanthk-decentrialz <hemanth.k@decentrialz.com>
2026-07-15 07:05:35 +0530

    Delete studyflow

--- studyflow/data/timer.ts
+++
@@ -1,5 +0,0 @@
-export const TIMER_DURATION = {
-  Pomodoro: 25 * 60,
-  "Short Break": 5 * 60,
-  "Long Break": 15 * 60,
-} as const;
--- studyflow/types/goal.ts
+++
@@ -1,7 +0,0 @@
-export interface Goal {
-  id: number;
-  title: string;
-  subject: string;
-  deadline: string;
-  progress: number;
-}
